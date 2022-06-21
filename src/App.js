43d9d13b9daa78d8b27f9de41dda2a011301658e
kerim <krmksmsite@gmail.com>
2022-06-21 15:02:07 +0300

    components are done.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-
-import './App.css';
-import { Component } from 'react';
-import Main from './components';
+import {useState, useEffect} from 'react'
+import "./App.css"
+import Form from './components/Form'
+import List from './components/List'
+import Footer from './components/Footer'
 
 function App() {
-  return (
-    <div className="App">
+
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("");
+  const [filtered, setFiltered] = useState([]);
+
+  useEffect(() => {
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFiltered(todos.filter(todo => todo.completed === true))
+        
+        break;
+        case "active":
+        setFiltered(todos.filter(todo => todo.completed === false))
+          
+        break;
     
-      <Main />
+      default:
+        setFiltered(todos) 
+        break;
+    }
+  }
+  filterHandler()
+  }, [todos,status])
+
+
+  return (
+    <div className='todoapp'>
+
+    <Form todos={todos} setTodos={setTodos} todo={todos.todo}/>
+
+    <ul className='todo-list'>
+      {filtered.map((todo) =>(
+        <List status={status} key={todo.id} todo={todo}
+         text = {todo.text} todos={todos} setTodos={setTodos}
+        />
+      ))}
+    </ul>
+    
+    <Footer
+          status= {status}
+          setStatus={setStatus}
+          todos={todos}
+          setTodos={setTodos}
+          />
+    
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Main() {
-  return (
-    <div>Merhaba</div>
-  )
-}
-
-export default Main
